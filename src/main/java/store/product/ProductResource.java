58cd04792602d8c7eb3ac20c0aa8c1135373e4bf
Humberto Sandmann <hsandmann@ieee.org>
2023-11-09 13:05:22 -0300

    eureka registered

--- src/main/java/store/product/ProductResource.java
+++ src/main/java/store/product/ProductResource.java
@@ -10,34 +10,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @CrossOrigin(origins = "*")
 @RestController
+@RequestMapping("/product")
 public class ProductResource {
 
     @Autowired
     private ProductService productService;
 
-    @GetMapping("/product")
+    @GetMapping("/")
     public List<ProductOut> list() {
         return productService.list().stream().map(ProductParser::to).toList();
     }
 
-    @GetMapping("/product/{id}")
+    @GetMapping("/{id}")
     public ProductOut get(@PathVariable(required = true) String id) {
         Product found = productService.find(id);
         return found == null ? null : ProductParser.to(found);
     }
 
-    @DeleteMapping("/product/{id}")
+    @DeleteMapping("/{id}")
     public void delete(@PathVariable(required = true) String id) {
         productService.delete(id);
     }
 
-    @PostMapping("/product")
+    @PostMapping("/")
     public ResponseEntity<Object> create(@RequestBody ProductIn in) {
+        // Padrao HateOAS
         return ResponseEntity.created(
                     ServletUriComponentsBuilder
                         .fromCurrentRequest()
