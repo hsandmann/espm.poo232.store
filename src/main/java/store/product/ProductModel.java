d880f8aa2c12267a6e96dd5975fb991f224a329e
Humberto Sandmann <hsandmann@ieee.org>
2023-11-27 18:44:47 -0300

    aditional notation

--- src/main/java/store/product/ProductModel.java
+++ src/main/java/store/product/ProductModel.java
@@ -6,10 +6,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "product")
+@EqualsAndHashCode(of = "id")
 @NoArgsConstructor
 public class ProductModel {
     
